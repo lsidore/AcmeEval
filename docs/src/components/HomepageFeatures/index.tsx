839b502c01d24d9d4d6994b91f9a0b86496e7555
lsidoree <isidore.mikorey-nilsson@pytheascapital.com>
2024-06-16 16:51:23 +0200

    Deploy website - based on 4111ebc33536f8cff34e25151bbfe971bf4a6ee5

--- docs/src/components/HomepageFeatures/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Features } from "./Features";
-import { Installation } from "./Installation";
-
-export const HomepageFeatures = () => (
-  <main className="flex-1">
-    <Installation />
-    <Features />
-  </main>
-);
